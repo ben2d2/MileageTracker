25e4b01af72e1cd7969434a7adb16ab5df01ca7a
Ben Bridges <ben2d2@gmail.com>
2012-11-27 23:39:38 -0600

    Add partials to admin views and dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,30 +4,38 @@ ActiveAdmin.register_page "Dashboard" do
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
     div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span "Welcome to Active Admin. This is the default dashboard page."
-        small "To add dashboard sections, checkout 'app/admin/dashboards.rb'"
-      end
+      
     end
 
     # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    
+    columns do
+        
+      column do
+        panel "Recent Job Logs" do
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+            table_for JobLog.limit(10) do
+                column :id
+                column :date do |job_log|
+                    job_log.date
+                end
+                column :client_id do |job_log|
+                      job_log.client.name
+                    end
+                column :subcontractor_id do |job_log|
+                      job_log.subcontractor.display_name
+                    end
+                column :miles
+                column :hours
+          end
+        end
+      end
+
+      column do
+        panel "Info" do
+          para "Welcome to ActiveAdmin."
+        end
+      end
+    end
   end # content
 end
